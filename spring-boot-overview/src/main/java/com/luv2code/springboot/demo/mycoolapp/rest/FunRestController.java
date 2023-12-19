b9525bff297bb843e2f28dabacbc7f66432f6291
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-12-19 23:41:38 +0700

    Spring Boot Demo

---
+++ spring-boot-overview/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2023. vnlemanhthanh.com
+ */
+
+package com.luv2code.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World";
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Rung a hard 5k!";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyForturn() {
+        return "Today is your lucky day.";
+    }
+
+}
+
+
