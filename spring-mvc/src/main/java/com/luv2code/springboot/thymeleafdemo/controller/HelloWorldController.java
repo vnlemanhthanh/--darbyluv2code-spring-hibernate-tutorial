9c6d53030e48e475afb99f2e26945971eebedd54
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-12-21 21:14:41 +0700

    Hello World Form and Model

---
+++ spring-mvc/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2023. vnlemanhthanh.com
+ */
+
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/showForm")
+    public String showForm() {
+        return "helloworld-form";
+    }
+
+    @RequestMapping("/processForm")
+    public String processForm() {
+        return "helloworld";
+    }
+}
