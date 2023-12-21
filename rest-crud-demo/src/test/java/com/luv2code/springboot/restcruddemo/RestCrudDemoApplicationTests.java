1598d2c90b6c77fb895bc0fdb976d63e6e817db4
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-12-21 07:35:45 +0700

    Rest API CRUD

---
+++ rest-crud-demo/src/test/java/com/luv2code/springboot/restcruddemo/RestCrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.restcruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCrudDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
