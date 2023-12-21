e7b2151c51e1aa57c68e3bd21a5f8c5a6ed98a73
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-12-21 18:00:18 +0700

    Spring MVC with Thymeleaf

--- spring-mvc/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/DemoController.java
+++ spring-mvc/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/DemoController.java
@@ -5,7 +5,18 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
 
 @Controller
 public class DemoController {
+
+    @GetMapping("/hello")
+    public String sayHello(Model model) {
+        model.addAttribute("theDate", new Date());
+        return "helloworld";
+    }
+
 }
