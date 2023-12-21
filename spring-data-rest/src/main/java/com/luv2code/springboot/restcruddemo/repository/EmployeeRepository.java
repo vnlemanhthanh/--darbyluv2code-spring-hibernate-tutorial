a43a4d325a3e07ef17267131d576813122f2c608
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-12-21 14:19:52 +0700

    Spring Data REST

--- spring-data-rest/src/main/java/com/luv2code/springboot/restcruddemo/repository/EmployeeRepository.java
+++ spring-data-rest/src/main/java/com/luv2code/springboot/restcruddemo/repository/EmployeeRepository.java
@@ -7,6 +7,7 @@ package com.luv2code.springboot.restcruddemo.repository;
 import com.luv2code.springboot.restcruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
