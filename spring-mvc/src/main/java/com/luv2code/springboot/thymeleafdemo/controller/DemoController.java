f7bb4f3047889a7c7af623ec72fdc7e823c94b82
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-12-21 17:52:49 +0700

    Spring MVC

---
+++ spring-mvc/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2023. vnlemanhthanh.com
+ */
+
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DemoController {
+}
