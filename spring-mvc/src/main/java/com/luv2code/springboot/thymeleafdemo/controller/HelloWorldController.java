a9607ef4b76086d7daace311c8b18586e0262922
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-12-21 22:15:31 +0700

    Add data to Model

--- spring-mvc/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ spring-mvc/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -4,7 +4,9 @@
 
 package com.luv2code.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,6 +19,16 @@ public class HelloWorldController {
 
     @RequestMapping("/processForm")
     public String processForm() {
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormV2")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+        String name = request.getParameter("studentName");
+        String result = "Yo! Yo! " + name.toUpperCase();
+
+        model.addAttribute("message", result);
+
         return "helloworld";
     }
 }
